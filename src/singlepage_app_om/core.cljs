f43ed1aae80ba6fa196156cecfa428a5dc3a14ea
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-08-25 22:35:18 -0400

    First commit

---
+++ src/singlepage_app_om/core.cljs
@@ -0,0 +1,15 @@
+(ns singlepage-app-om.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(def app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [app owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text app)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
